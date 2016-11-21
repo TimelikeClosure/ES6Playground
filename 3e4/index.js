21c1fb56bfc09b29b54d148b10fda0cffbd974fe
Tim Horist <TimelikeClosure@users.noreply.github.com>
2016-11-21 11:10:33 -0800

    Finished 3e4

---
+++ 3e4/index.js
@@ -0,0 +1,16 @@
+/* 
+Using map, create a new array that contains the distance / time value from each trip.  In other words, the new array should contain the (distance / time) value.  Assign the result to the variable 'speeds'.
+*/
+
+// START CODE
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+
+var speeds = trips.map(function(trip){
+    return trip.distance / trip.time;
+});
+// END CODE
+console.log(speeds);
