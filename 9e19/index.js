4e7ab52653e0502f40b84e74df2e29bcf8d3f0ea
Tim Horist <TimelikeClosure@users.noreply.github.com>
2016-11-23 15:41:02 -0800

    Finished 9e19

---
+++ 9e19/index.js
@@ -0,0 +1,9 @@
+/*
+Refactor the function to use template strings
+ */
+//  CODE START
+function doubleMessage(number) {
+  return `Your number doubled is ${2 * number}`;
+}
+//  CODE END
+console.log(doubleMessage(65));
