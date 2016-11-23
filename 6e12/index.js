11b6099e229e25ba1e7aeb48a50151e0f5247e9d
Tim Horist <TimelikeClosure@users.noreply.github.com>
2016-11-22 20:10:17 -0800

    Finished 6e12

---
+++ 6e12/index.js
@@ -0,0 +1,15 @@
+/*
+Given an array of users, return 'true' if every user has submitted a request form.  Assign the result to the variable 'hasSumbmitted'.
+ */
+//  CODE START
+var users = [
+  { id: 21, hasSubmitted: true },
+  { id: 62, hasSubmitted: false },
+  { id: 4, hasSubmitted: true }
+];
+
+var hasSubmitted = users.every(function(user){
+    return user.hasSubmitted;
+});
+//  CODE FINISH
+console.log(hasSubmitted);
