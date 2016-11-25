39ec2e2480e46396837fd2f48ea7caf52d01a6cb
Tim Horist <TimelikeClosure@users.noreply.github.com>
2016-11-25 08:03:29 -0800

    Finished 15_45

--- 15_45/index.js
+++ 15_45/index.js
@@ -11,6 +11,9 @@ function Toyota(options) {
     this.color = options.color;
 }
 Toyota.prototype.__proto__ = Car.prototype;
+// Or the lines below, because being convoluted is fun
+// Toyota.prototype = Object.create(Car.prototype);
+// Toyota.prototype.constructor = Toyota;
 Toyota.prototype.honk = function () { 
     return 'beep';
 };
